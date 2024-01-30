e5cb68e771cf120632a537fa445cb0c20f1f8077
Vladislav <v.skorokhod@aorion.aero>
2024-01-30 12:01:50 +0300

    changes

--- Gate_controllers/Garage_gate_controller_v2_0/Garage_gate_controller_v2_0.ino
+++ Gate_controllers/Garage_gate_controller_v2_0/Garage_gate_controller_v2_0.ino
@@ -1,8 +1,8 @@
-#define LED_pin A5
+#define LED_pin 8
 #define input_pin 3
 #define PWM_freq 500 // Hz
 #define open_pin 4
-#define close_pin 5
+#define close_pin 2
 unsigned long duration;
 #include "MyTimer.h";
 int current_button, prev_current_button;
@@ -12,6 +12,36 @@ svsTimer button_hold_tm;
 long button_hold_delay = 30000; // ms (время удержания кнопки)
 int output_driver_mode, prev_output_driver_mode;
 long PWM_period_us;
+byte prev_button_stat;
+bool prev_button_mode;
+
+svsTimer button_tm;
+svsTimer LED_tm;
+bool long_pressed = false;
+
+int read_buttons () {
+  int output_val = false;
+  byte val = 0;
+  for (int i = 0; i < 2; i++) bitWrite(val, i, !digitalRead(button_pin[i])); //1 - 2 - 4 - 8
+  if (val && !long_pressed) {
+    long_pressed = true;
+    button_tm.reset();
+  }
+  if (button_tm.ready(200) && long_pressed) {
+    if (val) output_val = val;
+    else output_val = 0;
+    long_pressed = false;
+    delay(100);
+  }
+  return output_val;  
+}
+
+void LED(bool val) {
+  static bool LED_stat;
+  if (LED_tm.ready(250)) LED_stat = !LED_stat;
+  if (val) digitalWrite(LED_pin, LED_stat);
+  else digitalWrite(LED_pin, LOW);
+}
 
 void setup() {
   pinMode(input_pin, INPUT);
@@ -35,16 +65,16 @@ void setup() {
 void loop() {
   prev_current_button = current_button; 
   current_button = read_buttons();
-  Serial.println(current_button);
   if (current_button) {
+    Serial.println(current_button);
     button_hold_tm.reset();
     if (BUTTON_MODE == true) {
       BUTTON_MODE = false;
-      delay(500);
+      delay(100);
     } else {
       BUTTON_MODE = true;
       output_driver_mode = current_button;
-      delay(500);
+      //delay(500);
     }
   }
   if (button_hold_tm.ready(button_hold_delay)) BUTTON_MODE = false;
@@ -52,12 +82,13 @@ void loop() {
     if (output_driver_mode == 1) {        // OPEN
       digitalWrite(open_pin, HIGH);
       digitalWrite(close_pin, LOW);
-      digitalWrite(LED_pin, HIGH);
+      //digitalWrite(LED_pin, HIGH);
     } else if (output_driver_mode == 2) { // CLOSE
       digitalWrite(open_pin, LOW);
       digitalWrite(close_pin, HIGH);
-      digitalWrite(LED_pin, HIGH);
+      //digitalWrite(LED_pin, HIGH);
     }
+    LED(HIGH);
   } else {
     duration = pulseIn(input_pin, HIGH, PWM_period_us * 2); //timeout 10ms
     int val = 100 * duration / PWM_period_us; // %
@@ -65,21 +96,21 @@ void loop() {
     if (val >= 80 && val < 95) {        // OPEN
       digitalWrite(open_pin, HIGH);
       digitalWrite(close_pin, LOW);
-      digitalWrite(LED_pin, HIGH);
+      LED(HIGH);
     } else if (val <= 20 && val > 5) {  // CLOSE
       digitalWrite(open_pin, LOW);
       digitalWrite(close_pin, HIGH);
-      digitalWrite(LED_pin, HIGH);
+      LED(HIGH);
     } else {                            // STOP
       digitalWrite(open_pin, LOW);
       digitalWrite(close_pin, LOW);
-      digitalWrite(LED_pin, LOW);
+     LED(LOW);
     }
   }
-}
 
-byte read_buttons () {
-  byte val = 0;
-  for (int i = 0; i < 2; i++) bitWrite(val, i, !digitalRead(button_pin[i])); //1 - 2 - 4 - 8
-  return val;  
+  if (prev_button_mode != BUTTON_MODE) {
+    Serial.println("CHANGES!");
+    delay(1000);
+  }
+  prev_button_mode = BUTTON_MODE;
 }
